cd12660efc825b4637ff5522fdb86460b9fafb59
Michael Spencer <michael.spencer1@g.austincc.edu>
2022-08-30 15:29:35 -0500

    add spawn new piece function

--- js/config.js
+++ js/config.js
@@ -1,7 +1,45 @@
 const settings = {
     dimensions: {
         unit: 30,
-        width: 14,
-        height: 25,
+        width: 13,
+        height: 20,
     }
 }
+
+let origin = {
+    x: undefined,
+    y: 0
+}
+
+if(settings.dimensions.width % 2 != 0) {
+    origin.x = settings.dimensions.width / 2 - 0.5
+} else {
+    origin.x = settings.dimensions.width / 2
+}
+
+const pieces = [
+    [
+        origin,
+        { x: origin.x, y: origin.y + 1 },
+        { x: origin.x + 1, y: origin.y + 1 },
+        { x: origin.x - 1, y: origin.y + 1 },
+    ],
+    [
+        origin,
+        { x: origin.x + 1, y: origin.y },
+        { x: origin.x + 2, y: origin.y },
+        { x: origin.x - 1, y: origin.y },
+    ],
+    [
+        origin,
+        { x: origin.x , y: origin.y + 1 },
+        { x: origin.x + 1 , y: origin.y },
+        { x: origin.x + 1 , y: origin.y + 1 },
+    ],
+    [
+        origin,
+        { x: origin.x + 1 , y: origin.y },
+        { x: origin.x - 1, y: origin.y },
+        { x: origin.x - 1 , y: origin.y + 1},
+    ],
+]
--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
 const $start = document.getElementById('start')
 const $gameBoard = document.getElementsByClassName('gameboard')[0]
 const model = {
-    boxes: []
+    boxes: [],
+    currentPiece: {
+        coors: []
+    }
 }
 $start.addEventListener('click', () => {
+    if(settings.dimensions.width < 5 || settings.dimensions.height < 7) {
+        console.error('invalid gameboard dimensions')
+        return
+    }
     $start.style.display = 'none'
     launch()
 })
@@ -13,17 +20,20 @@ function onPageLoad() {
     $gameBoard.style.height = settings.dimensions.height * settings.dimensions.unit + 'px'
 }
 
-function launch() { 
+function launch() {
     initModel()
     initDom()
+    spawnNewPeice()
+    //begin anmation function which draws so dont draw here
+    draw()
 }
 
 function initModel() {
-    const rows = settings.dimensions.height
     const cols = settings.dimensions.width
+    const rows = settings.dimensions.height
     let id = 0
-    for(let i = 0; i < cols; i++) {
-        for(let j = 0; j < rows; j++) {
+    for(let i = 0; i < rows; i++) {
+        for(let j = 0; j < cols; j++) {
             let box = {
                 id: id,
                 x: i,
@@ -36,20 +46,51 @@ function initModel() {
     }
 }
 
+
 function initDom() {
-    const rows = settings.dimensions.height
     const cols = settings.dimensions.width
+    const rows = settings.dimensions.height
     for(let i = 0; i < cols; i++) {
-        const $row = document.createElement('div')
+        const $col = document.createElement('div')
+        $col.classList.add('col')
         for(let j = 0; j < rows; j++) {
             const $box = document.createElement('div')
             $box.classList.add('box-main')
             $box.style.height = settings.dimensions.unit + 'px'
             $box.style.width = settings.dimensions.unit + 'px'
-            $row.appendChild($box)
+            $col.appendChild($box)
         }
-        $gameBoard.appendChild($row)
+        $gameBoard.appendChild($col)
     }
+}
+
+function getDomBox(box) {
+    return document.getElementsByClassName('col')[box.x].children[box.y]
+}
+
+function draw() {
+    model.boxes.forEach(box => {
+        if(box.color) {
+            getDomBox(box).style.background = box.color
+        }
+    })
+}
+
+
+
+function spawnNewPeice() {
+    // if(cant spawn bc pieces in the way) {
+    //     gameover()
+    // })
+    const randomPiece = pieces[Math.floor(Math.random() * (1 + 3))]
+    randomPiece.forEach(coorPair => {
+        console.log(coorPair)
+        model.boxes.forEach(box => {
+            if(coorPair.x == box.x && coorPair.y == box.y) {
+                box.color = 'blue'
+            }
+        })
+    })
 }
 
 onPageLoad()
